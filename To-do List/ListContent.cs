982eb77ef841aa5a212dd483120888c85a534ef8
3oyGuapo <fdafdaq@gmail.com>
2025-08-19 19:20:30 +0100

    Third Commit: Adding class for storing todo list data and combine with existing methods

---
+++ To-do List/ListContent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_do_List
+{
+    public class ListContent
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public ListContent(string description)
+        {
+            Description = description;
+            IsCompleted = false;
+        }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}
