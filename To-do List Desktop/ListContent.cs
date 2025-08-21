77ba02cc5b22114093de189984bab12cb2610d5c
3oyGuapo <fdafdaq@gmail.com>
2025-08-21 18:19:02 +0100

    feat(ui): Implement initial WPF app for Todo list console app
    Features implemented:
    Display box showing todo lists
    Input box for inputing content
    Add button that adds content to the list
    Delete button that removes content
    Tick box showing status of completion
    Data binding between display box and ObservableCollection

---
+++ To-do List Desktop/ListContent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace To_do_List
+{
+    public class ListContent : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _description;
+        private bool _isCompleted;
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    OnPropertyChanged(nameof(Description));
+                }
+            }
+        }
+
+        public bool IsCompleted
+        {
+            get { return _isCompleted; }
+            set
+            {
+                if (_isCompleted != value)
+                {
+                    _isCompleted = value;
+                    OnPropertyChanged(nameof(IsCompleted));
+                }
+            }
+        }
+        public ListContent(string description)
+        {
+            _description = description;
+            _isCompleted = false;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}
---
+++ To-do List Desktop/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using To_do_List;
+
+namespace To_do_List_Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ListContent> TodoList { get; set; }
+        public ListContent SelectedList { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            TodoList = new ObservableCollection<ListContent>();
+            this.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string description = inputTextBox.Text;
+
+            if (!string.IsNullOrWhiteSpace(description))
+            {
+                TodoList.Add(new ListContent(description));
+                inputTextBox.Text = "";
+            }
+            
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedList != null)
+            {
+                TodoList.Remove(SelectedList);
+            }
+            else
+            {
+                MessageBox.Show("Delete fail, selete a valid list to delete,");
+            }
+        }
+    }
+}
